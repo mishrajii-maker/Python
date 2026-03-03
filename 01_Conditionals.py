fce98bcb4bd243e169f0cd4c7a23b242e02c424b
mishrajii-maker <mishrajiiofficial05@gmail.com>
2026-03-04 01:57:57 +0530

    Add files via upload

---
+++ 01_Conditionals.py
@@ -0,0 +1,23 @@
+# #If else
+
+# a= int(input("Enter your age: "))
+
+# if(a>=18):
+#     print("You are above the age of coscent")
+#     print("Good for you")
+
+# else:
+#     print("You are below the age of consent")
+
+
+# print("End of Program")
+
+
+# Write a program to print yes when the age entered by the user is greater than or equal to 18
+
+a= int(input("Enter the number of age: "))
+
+if(a>=18):
+    print("Yes")
+else:
+    print("NO")
---
+++ 02_if_elif_else_ladder.py
@@ -0,0 +1,20 @@
+#If elif else ladder 
+
+a= int(input("Enter your age: "))
+
+if(a>=18):
+    print("You are above the age of coscent")
+    print("Good for you")
+
+elif(a<0):
+    print("You are entering an invalid negativ age")
+
+elif(a==0):
+    print("You are entering 0 which is not a valid age")
+
+else:
+    print("You are below the age of consent")
+
+
+print("End of Program")
+
