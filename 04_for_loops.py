3f9795785756da869bcd1e75cdaee2f957909f84
mishrajii-maker <mishrajiiofficial05@gmail.com>
2026-03-04 01:58:42 +0530

    Add files via upload

---
+++ 04_for_loops.py
@@ -0,0 +1,4 @@
+for i in range(4): # 0 se lekr n-1 tk jata hai
+    print(i)
+
+    
---
+++ 08_Pass.py
@@ -0,0 +1,9 @@
+for i in range(645):
+    pass    # Pass likh dene se kuchh bhi nhi likhunga aur isko itne me chhod k dusre ko kr lunga tb bhi error nhi ayega
+
+
+
+i=0
+while(i<45):
+    print(i)
+    i+=1
