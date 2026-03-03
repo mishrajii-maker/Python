767df5d989d7870ad387f10954d3be6584fdee67
mishrajii-maker <mishrajiiofficial05@gmail.com>
2026-03-04 01:55:56 +0530

    Add files via upload

---
+++ 03_str_function.py
@@ -0,0 +1,6 @@
+name ="Harry"
+
+print(len(name))
+print(name.endswith("rry")) #rry se end se bta rha isliye true bta rha 
+print(name.startswith("Har")) #agr "Har" se start ho rha to true bta rha 
+print(name.capitalize()) #pahle letter ko capital kr deta hai
---
+++ 04_escape_seq.py
@@ -0,0 +1,6 @@
+a = "harray is a good boy\n but not a bad boy" #\n se new line a jati hai 
+print(a)
+
+
+a = "harray is a good \"boy\"\n but not a bad boy" #\"boy\" backslas ke sath doublecote lgane se uss word me doblecote lga rhega 
+print(a)
